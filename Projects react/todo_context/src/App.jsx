a156269637bfc2e1c0a1179dea3b8d985734e655
imamanjangra <imamanjangra@gmail.com>
2025-09-25 21:42:29 +0530

    add new file to learn context

---
+++ Projects react/todo_context/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='bg-red-400'>hello</h1>
+    </>
+  )
+}
+
+export default App
