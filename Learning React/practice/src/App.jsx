305c24bb194d2b1cc08638e1a80879822828a2e2
imamanjangra <imamanjangra@gmail.com>
2025-10-01 00:26:56 +0530

    done all clear and count btn

--- Learning React/practice/src/App.jsx
+++ Learning React/practice/src/App.jsx
@@ -1,16 +1,19 @@
-import { useEffect, useState } from 'react'
+import {  useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [value, setValue] = useState("")
   const [tasks, setTasks] = useState([])
+ 
+  
+  const clearAll = () => {
+    setTasks([])
+  }
+  
+console.log(tasks)
+
 
   
-
-
-  const takeValue = (e) => {
-    setValue(e.target.value)
-  }
 
   const formsbmt = (e) => {
     e.preventDefault()
@@ -46,14 +49,18 @@ function App() {
     
   } , [tasks])
 
+  
+
+
   return (
     <div className='flex flex-col justify-center items-center mt-20 gap-10'>
       <form onSubmit={formsbmt} className='flex gap-5'>
+        <p className='h-12 px-3 border rounded-2xl text-center flex items-center' >No of task = {tasks.length} </p>
         <input
           type="text"
           className='border h-12 w-72 rounded-2xl hover:bg-blue-50 text-2xl pl-2'
           value={value}
-          onChange={takeValue}
+         onChange={(e) => setValue(e.target.value)}
         />
         <button
           className='h-12 px-5 rounded-2xl bg-green-500 hover:bg-green-400 text-white font-bold'
@@ -65,33 +72,49 @@ function App() {
 
       <ul className='flex flex-col gap-3 w-72'>
         {tasks.map((task, i) => (
-          <li
-            key={i}
-            className='h-12 border rounded-2xl flex items-center justify-between px-3 bg-white shadow'
-          >
-            <div className='flex items-center gap-2'>
-              <input
-                type='checkbox'
-                onChange={() => crossTask(i)}
-                checked={task.completed}
-                className='w-5 h-5 accent-blue-500'
-              />
-              <span
-                className={`text-lg ${task.completed ? "line-through text-gray-400" : "text-gray-800"}`}
+              <li
+                key={i}
+                className='h-12 border rounded-2xl flex items-center justify-between px-3 bg-white shadow'
               >
-                {task.text}
-              </span>
-            </div>
+                <div className='flex items-center gap-2'>
+                   <input
+                    type='checkbox'
+                    onChange={() => crossTask(i)}
+                    checked={task.completed}
+                    className='w-5 h-5 accent-blue-500'
+                  />
 
-            <button
-              className='text-red-500 font-bold hover:text-red-400'
-              onClick={() => deleteTask(i)}
-            >
-              X
-            </button>
-          </li>
+                  <span
+                    className={`text-lg ${task.completed ? "line-through text-gray-400" : "text-gray-800"}`}
+                    
+                  >
+                    {task.text}
+                  </span>
+
+                </div>
+
+                <div className='flex gap-2'>
+                  <button
+                    onClick={() => Edittask(i)}
+                    className='h-8 px-3 rounded-2xl bg-yellow-400 hover:bg-yellow-300 text-white font-semibold'
+                  >
+
+                    Edit
+                  </button>
+                  <button
+                    className='h-8 px-3 rounded-2xl bg-red-500 hover:bg-red-400 text-white font-bold'
+                    onClick={() => deleteTask(i)}
+                  >
+                    X
+                  </button>
+                </div>
+      </li>
+
         ))}
       </ul>
+      <button  className='h-10 px-3 border rounded-2xl'
+      onClick={clearAll}
+      >Clear ALL</button>
     </div>
   )
 }
